b41aafef9d11784c182b33a6aa16693738318141
Caitlin Riley <130196572+katyushkamilashka@users.noreply.github.com>
2023-04-10 19:11:03 -0400

    Update PropertyData.java

--- src/edu/upenn/cit594/util/PropertyData.java
+++ src/edu/upenn/cit594/util/PropertyData.java
@@ -4,4 +4,27 @@ public class PropertyData {
     public double marketValue;
     public double totalLivableArea;
     public int zipCode;
+    
+    /**
+    * 3 arg constructor
+    * @param market value as double
+    * @param liveable area as double
+    * @param zipcode as int
+    */
+    public PropertyData(double mValue, double area, int zip) {
+        marketValue = mValue;
+        totalLivableArea = area;
+        zipCode = zip;
+    }
+    
+     /**
+    * 3 arg constructor
+    * @param market value as String
+    * @param liveable area as String
+    * @param zipcode as String
+    */
+    public PropertyData(String mValue, String area, String zip) {
+       
+    }
+    
 }
